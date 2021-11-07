f341b8ea9203e5e436515df573602131f2de9b32
taisei6 <59239865+taisei6@users.noreply.github.com>
2021-11-07 17:44:09 +0900

    バルブとがっちゃんこ

--- Arduino/motorfader/motorfader.ino
+++ Arduino/motorfader/motorfader.ino
@@ -21,47 +21,48 @@ const int mf_pin = A1;
 
 int sli_v,mf_v = 0;
 
-
+const int flg0Pin = 10;         // the number of the pushflg pin
+const int ledPin = LED_BUILTIN;  // the number of the LED pin
+int flg0 = 0; 
 
 void setup()
 {
+ pinMode(ledPin, OUTPUT);
+ pinMode(flg0Pin, INPUT);
  
  Serial.begin(9600);
  
- forward(motor1,motor2, 200);
- delay(500);
- brake(motor1,motor2);
- delay(100);
+// forward(motor1,motor2, 200);
+// delay(500);
+// brake(motor1,motor2);
+// delay(100);
 }
 
 
-void loop()
-{
+void loop(){
+  
+  flg0 = digitalRead(flg0Pin);
   sli_v = analogRead(s_pin);
   mf_v = analogRead(mf_pin);
   Serial.print(sli_v);
   Serial.print("___");
   Serial.println(mf_v);
-
-  if(mf_v < sli_v){
-    motor1.drive(250,15);
-//    motor1.brake();
-  }
   
-  if(mf_v > sli_v){
-    motor1.brake();
-  }
-
-//  if(mf_v > sli_v){
-//    forward(motor1,motor2, 50);
-////   delay(100);
-//    }
-//  if(mf_v < sli_v){
-//    back(motor1,motor2, 50);
-////   delay(100);
-//    }
+  if (flg0 == HIGH) {
     
-//   brake(motor1,motor2);
-//   delay(100);
+    if(mf_v < sli_v){
+      motor1.drive(250,10);
+    motor1.brake();
+    }else{
+     motor1.brake();
+    }
+    digitalWrite(ledPin, HIGH);
+    
+  } else {
+    if(mf_v > sli_v){
+     motor1.brake();
+    }
+    digitalWrite(ledPin, LOW);
+  }
   
 }
